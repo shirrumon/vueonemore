a3aca13e17a7aeba3636f84a36f45c21ca1230f5
shirumon <shirumoni@gmail.com>
2021-04-25 13:13:23 +0200

    some change in main and pacage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+const PORT = process.env.PORT || 80
+App.listen(PORT, () => {
+  console.log("Server has been started...")
+})
 createApp(App).use(router).mount('#app')
